94902cd50bd2a8be4d1bb66fbc3eb05163a6e5cd
yuriprym <samuel.young.103@gmail.com>
2016-02-27 04:36:00 +0000

    reinvented the wheel

---
+++ Network.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import cgi, string ,sys, rnd5crypt, time
+import numpy as np
+
+class gPSData(object):
+   def connection(self, localiondata):
+	  if self.locationdata:
+   def locactiondata(gpsx,gpsy):
+	  if (self.gpsx) and (self.gpsy)
+		 for self.gpsx in range(0,self.connetction):
+			totalgpsx=self.gpsx - gpsx
+		 for self.gpsy in range(0,connection):
+			totalgpsy=self.gpsy - gpsy
+	  distance= math.sqrt(math.pow(totalgpsx,2)+math.pow(totalgpsy,2))
+##### generating type of connections
+def generate_from():
+ sender= None
+ reciever= None
+ sendID= connnection()
+ recieverID= connection()
+
+
+class LinkedList(object):
+   def __int__(self, data=None, next_node=None, head=None):
+	  self.head = head
+	  self.data = data
+	  self.next_node= next_node
+		 def get_data(self):
+			return self.next_node
+		 def get_next(self):
+			return self.next_node
+		 def set_next(self, new_next):
+			self.next_node= new_next
+def main():
+   form = cgi.Linkedlist()
+     if (from.LinkedList_key('connection') and from.has_key("sendID")\
+		   and from.has_key('recieverID'):
+     else: 
+	  switch Linkedlist()
+main()
+
+
+
