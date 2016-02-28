ea145a716850faf90cbd696e97970843cacf7780
yuriprym <samuel.young.103@gmail.com>
2016-02-28 01:21:52 -0500

    just add longtitude and latitude data point within a rough appromixationg

--- Barkler/vcu.barkler.edu/src/main/java/edu/vcu/barkler/barkler/Home.java
+++ Barkler/vcu.barkler.edu/src/main/java/edu/vcu/barkler/barkler/Home.java
@@ -1,9 +1,13 @@
 package edu.vcu.barkler.barkler;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +16,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.*;
 import java.net.*;
@@ -25,7 +33,11 @@ public class Home extends AppCompatActivity {
     private Socket client;
 
 
-    @Override
+    LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    double longitude=location.getLongitude();
+    double latitude=location.getLatitude();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
@@ -33,6 +45,9 @@ public class Home extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Button fab = (Button) findViewById(R.id.fab);
         Button fab2 = (Button) findViewById(R.id.fab2);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void profile(View view) {
@@ -40,8 +55,7 @@ public class Home extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void searchForDogs(View view)
-    {
+    public void searchForDogs(View view) {
         Intent searchForDogs = new Intent(this, searchForTheDoge.class);
         startActivity(searchForDogs);
 
@@ -69,6 +83,46 @@ public class Home extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Home Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://edu.vcu.barkler.barkler/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client2, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Home Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://edu.vcu.barkler.barkler/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client2, viewAction);
+        client2.disconnect();
     }
 }
 
